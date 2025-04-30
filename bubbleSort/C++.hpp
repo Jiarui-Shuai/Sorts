779fb6862950e8236aac33b6c685fe8e31afccc8
Jiarui-Shuai <sjr_3@outlook.com>
2025-04-30 22:25:45 +0800

    Upload Something

---
+++ bubbleSort/C++.hpp
@@ -0,0 +1,15 @@
+// This is the implementation of bubble sort in C++
+
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    } 
+}       
